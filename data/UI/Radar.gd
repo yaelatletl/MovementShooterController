05fca5e4e0c588166cd4bc0b93b1641b5b2fc164
Yael Atletl <yael.atletl@gmail.com>
2022-03-26 04:16:21 -0600

    initial frame for radar

---
+++ data/UI/Radar.gd
@@ -0,0 +1,16 @@
+tool
+extends ColorRect
+
+export(Color) var background = Color.aliceblue
+export(Color) var border = Color.black
+export(int) var border_resolution = 360
+export(int) var border_width = 1
+
+func _ready():
+	rect_size = get_parent().rect_size
+func _process(delta):
+	update()
+
+func _draw():
+	draw_circle(rect_size/2, min(rect_size.x/3, rect_size.y/3), background)
+	draw_arc (rect_size/2, min(rect_size.x/3, rect_size.y/3), 0, 360, border_resolution, border, border_width, true)
