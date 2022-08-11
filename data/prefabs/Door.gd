e1e82453ba15fead26dd6987c52a0ccf9c9e1901
Yael Atletl <yael.atletl@gmail.com>
2022-08-11 03:55:22 -0500

    Door works now

--- data/prefabs/Door.gd
+++ data/prefabs/Door.gd
@@ -11,13 +11,16 @@ var current_vel = 1
 var triggerable = true
 var closing = false
 
+export(float) var push_strenght = 150
+
 func interaction_triggered(interactor_body : Spatial):
 	if not triggerable:
 		return
 	var push_vec = Vector3.ZERO
+	wake_up()
 	if is_instance_valid(interactor_body):
 		#push_vec = hinge.global_transform.basis.z.direction_to(interactor_body.global_transform.origin).cross(Vector3.UP)
-		push_vec = interactor_body.global_transform.origin.direction_to(blade.global_transform.origin)
+		push_vec = interactor_body.global_transform.origin.direction_to(blade.global_transform.origin).normalized()
 	current_vel *= -1
 	triggerable = false
 	
@@ -30,12 +33,11 @@ func interaction_triggered(interactor_body : Spatial):
 		#hinge.set_flag(hinge.FLAG_ENABLE_MOTOR, true)
 	#else:
 	if closing:
-		blade.apply_central_impulse(350*-push_vec)
+		blade.apply_central_impulse(push_strenght * -push_vec)
 	else:
-		blade.apply_central_impulse(250* push_vec)
+		blade.apply_central_impulse(push_strenght * push_vec)
 
-	#hinge.set_param(hinge.PARAM_LIMIT_LOWER, -5)
-	#hinge.set_param(hinge.PARAM_LIMIT_UPPER, 5)
+
 	yield(get_tree().create_timer(1.0), "timeout")
 	#hinge.set_param(hinge.PARAM_LIMIT_UPPER, 90)
 	#hinge.set_param(hinge.PARAM_LIMIT_LOWER, -90)
@@ -47,13 +49,24 @@ func _physics_process(delta):
 		return
 	#print(blade.rotation_degrees.y, blade.linear_velocity.length())
 	if abs(blade.rotation_degrees.y) < 25:
-		if is_zero_approx(abs(blade.rotation_degrees.y)) and closing:
+		if is_zero_approx(abs(blade.linear_velocity.y)): 
+			stiffen_and_sleep()
+			print("sleeping")
+		if closing:
 			blade.linear_velocity = lerp(blade.linear_velocity, Vector3.ZERO, delta)
 			blade.angular_velocity = lerp(blade.angular_velocity, Vector3.ZERO, delta)
+			blade.rotation_degrees.y = lerp(blade.rotation_degrees.y, 0, delta)
+			print(rotation_degrees.y)
 		blade.linear_velocity -= 0.1*delta*blade.linear_velocity.normalized()
 		blade.angular_velocity -= 0.1*delta*blade.angular_velocity.normalized()
 
 
-func close_door():
+func stiffen_and_sleep():
 	hinge.set_flag(hinge.FLAG_ENABLE_MOTOR, true)
-	hinge.set_param(hinge.PARAM_MOTOR_TARGET_VELOCITY, current_vel)
+	hinge.set_param(hinge.PARAM_LIMIT_LOWER, -2)
+	hinge.set_param(hinge.PARAM_LIMIT_UPPER, 2)
+
+func wake_up():
+	hinge.set_flag(hinge.FLAG_ENABLE_MOTOR, false)
+	hinge.set_param(hinge.PARAM_LIMIT_LOWER, -90)
+	hinge.set_param(hinge.PARAM_LIMIT_UPPER, 90)
