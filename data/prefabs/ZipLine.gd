18c04df4e3a850db6d15ad2bf8d1317887510eff
Yael Atletl <yael.atletl@gmail.com>
2022-08-21 03:30:08 -0500

    Snap player to zipline

--- data/prefabs/ZipLine.gd
+++ data/prefabs/ZipLine.gd
@@ -10,6 +10,7 @@ var zipline_direction = Vector3.ZERO
 var current_bodies = []
 var linked_bodies = []
 var area : Area
+export(float) var y_offset = 0.7
 #little struct here:
 #{
 #	body: body,
@@ -36,6 +37,7 @@ func _ready():
 	interactable.add_child(area)
 	add_child(interactable)
 	area.global_transform.origin = start.global_transform.origin
+	area.global_translate(Vector3(0, -y_offset, 0))
 	interactable.message = "Press E to use Zipline"
 	area.translation += zipline_direction*(distance / 2)
 	area.look_at(end.global_transform.origin, Vector3(0, 1, 0))
@@ -48,6 +50,7 @@ func _on_interacted_successfully(body):
 		struct["zip_direction"] = body.head_basis.z.dot(zipline_direction)
 		linked_bodies.append(body)
 		current_bodies.append(struct)
+		body.global_transform.origin = get_point_to_snap(body.global_transform.origin)
 
 func _on_body_exited(body):
 	if not body in linked_bodies:
@@ -66,19 +69,35 @@ func _on_body_deattached(struct):
 			current_bodies.remove(i)
 			break
 
+func get_point_to_snap(actor_origin : Vector3):
+	var distance_to_start = actor_origin.distance_to(start.global_transform.origin)
+	var distance_to_end = actor_origin.distance_to(end.global_transform.origin)
+	var distance_to_start_end = start.global_transform.origin.distance_to(end.global_transform.origin)
+	var nearest = min(distance_to_start, distance_to_end)
+	var intersection = Vector3.ZERO
+	if nearest == distance_to_start:
+		intersection = start.global_transform.origin
+		intersection += zipline_direction.normalized()*(nearest)
+	else:
+		intersection = end.global_transform.origin
+		intersection -= zipline_direction.normalized()*(nearest)
+	return intersection
 
 func _physics_process(delta):
 	for struct in current_bodies:
 		if not area.overlaps_body(struct["body"]):
 				_on_body_deattached(struct)
 				return
+				
 		if struct["zip_direction"] > 0:
 			if struct["body"].global_transform.origin.distance_to(start.global_transform.origin) < 1:
 				_on_body_deattached(struct)
 			else:
-				struct["body"].linear_velocity = zipline_direction * -10 
+				#struct["body"].linear_velocity = zipline_direction * -10 
+				struct["body"].linear_velocity = struct["body"].global_transform.origin.direction_to(start.global_transform.origin) * 10
 		else:
 			if struct["body"].global_transform.origin.distance_to(end.global_transform.origin) < 1:
 				_on_body_deattached(struct)
 			else:
-				struct["body"].linear_velocity = zipline_direction * 10 
+				#struct["body"].linear_velocity = zipline_direction * 10 
+				struct["body"].linear_velocity = struct["body"].global_transform.origin.direction_to(end.global_transform.origin) * 10
