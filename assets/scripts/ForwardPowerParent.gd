eda7d5956a1e9497196b2265183ab336983cc9fb
Yael Atletl <yael.atletl@gmail.com>
2023-02-12 22:26:22 -0600

    some assets

--- assets/scripts/ForwardPowerParent.gd
+++
@@ -1,7 +0,0 @@
-# This script can be attached to a PhysicsBody3D to make power feed to its parent
-
-extends Node3D
-
-
-func recv_power(powered: bool) -> void:
-	get_parent().recv_power(powered)
--- assets/scripts/Util.gd
+++
@@ -1,8 +0,0 @@
-# Static class (namespace) with utility functions
-
-# Dampen the length of a vector if it is above a maximum
-static func dampen_vector(vec: Vector3, max_len: float) -> Vector3:
-	if vec.length() > max_len:
-		return vec.normalized() * max_len
-	else:
-		return vec
